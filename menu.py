a480a113354f01c4ffcabf1a322bd5584c579547
codedByBojan <jevdja990@gmail.com>
2025-05-26 21:14:09 +0200

    Returns all the names of the available menu items

--- menu.py
+++ menu.py
@@ -19,3 +19,12 @@ class Menu:
             MenuItem(name="cappuccino", water=250, milk=50, coffee=24, cost=3),
         ]
 
+    def get_items(self):
+        """Returns all the names of the available menu items"""
+        available = ""
+        for item in self.menu:
+            available += f"{item.name}/"
+        return available
+    
+    
+
