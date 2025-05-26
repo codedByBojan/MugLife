67bb0a5d1c4d8e6a2ac63bcf4883cc70674399a4
codedByBojan <jevdja990@gmail.com>
2025-05-26 21:40:04 +0200

    Adding class coffe maker and method report

---
+++ coffee_maker.py
@@ -0,0 +1,14 @@
+class CoffeeMaker:
+    """Models the machine that makes the coffee"""
+    def __init__(self):
+        self.resources = {
+            "water": 300,
+            "milk": 200,
+            "coffee": 100,
+        }
+    
+    def report(self):
+        """Prints a report of all resources."""
+        print(f"Water: {self.resources['water']}ml")
+        print(f"Milk: {self.resources['milk']}ml")
+        print(f"Coffee: {self.resources['coffee']}g")
