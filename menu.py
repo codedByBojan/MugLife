3923b4a9acc6e75174ae5b419060ff8b7963a8fd
codedByBojan <jevdja990@gmail.com>
2025-05-26 20:57:51 +0200

    Creating menu items

---
+++ menu.py
@@ -0,0 +1,23 @@
+class MenuItem:
+    """Models each Menu item."""
+    def __init__(self, name, cost, ingredients):
+        """Initialize name, cost and ingredients for coffee"""
+        self.name = name
+        self.cost = cost
+        self.ingredients = {
+            "water": water,
+            "milk": milk,
+            "coffee": coffee
+        }
+
+
+class Menu:
+    """Display of the coffee menu from the machine"""
+    def __init__(self):
+        self.menu = [
+            MenuItem(name="latte", water=200, milk=150, coffee=24, cost=2.5),
+            MenuItem(name="espresso", water=50, milk=0, coffee=18, cost=1.5),
+            MenuItem(name="cappuccino", water=250, milk=50, coffee=24, cost=3)
+        ]
+
+
